589b014234b2057be8a3a71b3c5305596be685af
Aman Tejaswi <atejaswi@aegaeon.crc.nd.edu>
2024-04-25 13:39:24 -0400

    Added Hydrology (NWM Coupling & MTC changes), correction for river boundary forcing to account for projection

--- cmake/padcirc.cmake
+++ cmake/padcirc.cmake
@@ -61,7 +61,9 @@ if(BUILD_PADCIRC)
       ${CMAKE_CURRENT_SOURCE_DIR}/src/couple2baroclinic3D.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/gl2loc_mapping.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/internaltide.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/subgridLookup.F)
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/subgridLookup.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/river.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/owi_rain.F)
 
   add_executable(padcirc ${PADCIRC_SOURCES})
 
