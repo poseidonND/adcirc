a7e98cf575981b194a47da7176299a7c627e4f6a
Aman Tejaswi <atejaswi@aegaeon.crc.nd.edu>
2024-09-14 00:05:14 -0400

    Adding Baroclinic Current Synthesis Code

--- cmake/padcirc.cmake
+++ cmake/padcirc.cmake
@@ -65,7 +65,8 @@ if(BUILD_PADCIRC)
       ${CMAKE_CURRENT_SOURCE_DIR}/src/sun_moon_system.F90
       ${CMAKE_CURRENT_SOURCE_DIR}/src/subgridLookup.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/river.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/owi_rain.F)
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/owi_rain.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/synthesis.F90)
   if(NETCDF_WORKING)
     set(PADCIRC_SOURCES
         ${PADCIRC_SOURCES}
